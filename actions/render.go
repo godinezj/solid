92ca9361ded481f1645c36c4c8a7e47a5055df99
Javier Godinez <godinezj@gmail.com>
2019-06-20 07:26:42 -0700

    Added renderer for templating

--- actions/render.go
+++ actions/render.go
@@ -2,10 +2,14 @@ package actions
 
 import (
 	"github.com/gobuffalo/buffalo/render"
+	"github.com/gobuffalo/packr/v2"
 )
 
 var r *render.Engine
 
 func init() {
-	r = render.New(render.Options{})
+	r = render.New(render.Options{
+		// Box containing all of the templates:
+		TemplatesBox: packr.New("app:templates", "../templates"),
+	})
 }
